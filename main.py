cd29c99c76e9f3acdc09f2374c88dcdc20cfaaf4
Anand Thamban <anand.thamban@hekno25.org>
2024-11-04 16:29:48 -0600

    Added comments and Escape closes window

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import pygame
 import sys
 
+# Initialize pygame stuff
 pygame.init()
 
-size = width, height = 1280, 720
+# How big we want our window in pixels
+# width, height
+size = 1280, 720
 
+# Open a window with our desired size
 screen = pygame.display.set_mode(size)
 
+# Our main game loop
 while True:
+    # Stop the program if the window is closed or
+    # escape is pressed on the keyboard
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             sys.exit()
 
     screen.fill(pygame.Color(0, 0, 0))
