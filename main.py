958d2f47095cc43f96d3502b6216ed4977b64bde
Anand Thamban <anand.thamban@hekno25.org>
2024-11-18 19:02:39 -0600

    Reorganize main.py into a Game class

--- main.py
+++ main.py
@@ -1,26 +1,60 @@
 import pygame
 import sys
 
-# Initialize pygame stuff
-pygame.init()
 
-# How big we want our window in pixels
-# width, height
-size = 1280, 720
+class Game:
+    """A class representing our Game"""
 
-# Open a window with our desired size
-screen = pygame.display.set_mode(size)
+    def __init__(self):
+        """Initializes our game"""
 
-# Our main game loop
-while True:
-    # Stop the program if the window is closed or
-    # escape is pressed on the keyboard
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            sys.exit()
+        # Initialize pygame stuff
+        pygame.init()
 
-    screen.fill(pygame.Color(0, 0, 0))
+        # How big we want our window in pixels
+        # width, height
+        self.size = 1280, 720
 
-    pygame.display.flip()
+        # Open a window with our desired size
+        self.window_surface = pygame.display.set_mode(self.size)
+        # Set window name
+        pygame.display.set_caption("Angry Fly")
+
+        # Used for consistent framerate
+        self.clock = pygame.time.Clock()
+        self.framerate = 60
+
+        # Sprite group
+        self.all_sprites = pygame.sprite.Group()
+
+    def run(self):
+        """
+        What will happen each frame of the program running
+        """
+
+        last_time = self.clock.get_time()
+        while True:
+            # Delta time
+            dt = self.clock.get_time() - last_time
+            last_time = self.clock.get_time()
+
+            # Event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    sys.exit()
+
+            # Game logic
+            self.window_surface.fill(pygame.Color(173, 216, 230))
+
+            pygame.display.flip()
+
+            self.clock.tick(self.framerate)
+
+
+# Main function
+if __name__ == "__main__":
+    game = Game()
+
+    game.run()
